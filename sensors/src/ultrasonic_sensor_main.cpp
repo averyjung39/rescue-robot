c69ad97dbdd763cc3afb99c8afab727bcf521da8
Avery Jung <14814535+averyjung39@users.noreply.github.com>
2019-02-27 23:53:37 -0500

    Created ultrasonic_sensor node to get ultrasonic sensor data and publish the converted distance in cm (#12)

---
+++ sensors/src/ultrasonic_sensor_main.cpp
@@ -0,0 +1,58 @@
+#include <ros/ros.h>
+#include <ros/time.h>
+#include <ros/duration.h>
+#include <string>
+
+#include "sensors/Ultrasonic.h"
+#include "sensors/GPIOClass.h"
+#include "topics/topics.h"
+
+int main (int argc, char **argv)
+{
+    ros::init(argc, argv, "ultrasonic_sensor");
+
+    ros::NodeHandle nh;
+    ros::Publisher ult_data_pub = nh.advertise<sensors::Ultrasonic>(topics::ULTRASONIC_TOPIC, 1);
+
+    GPIOClass* gpio18 = new GPIOClass("18"); // trig
+    gpio18->export_gpio();
+    gpio18->setdir_gpio("out");
+
+    GPIOClass* gpio24 = new GPIOClass("24"); // echo
+    gpio24->export_gpio();
+    gpio24->setdir_gpio("in");
+
+    long counter = 0;
+    string inputstring;
+    float distance = 0;
+    ros::Time start = ros::Time::now();
+    ros::Time finish = ros::Time::now();
+
+    sensors::Ultrasonic ult_data_cm;
+
+    while(ros::ok())
+    {
+        counter = 0;
+        gpio18->write_gpio("1");
+        ros::Duration(0.00001).sleep();
+        gpio18->write_gpio("0");
+
+        gpio24->read_gpio(inputstring);
+        while(inputstring == "0")
+        {
+            gpio24->read_gpio(inputstring);
+            start = ros::Time::now();
+        }
+        while(inputstring == "1")
+        {
+            gpio24->read_gpio(inputstring);
+            finish = ros::Time::now();
+        }
+
+        ros::Duration elapsed = finish - start;
+        // convert duration to seconds
+        distance = (elapsed.toSec() * 34300) / 2;
+        ult_data_cm.data[0] = distance;
+        ult_data_pub.publish(ult_data_cm);
+    }
+}
