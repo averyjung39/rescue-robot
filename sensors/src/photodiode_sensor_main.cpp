1449d79f8d34aa0b0b46b3c8b11ee7444e8367f6
Avery Jung <yhj.ser@gmail.com>
2019-03-05 17:42:13 -0500

    Create a vector of GPIOClass pointers

--- sensors/src/photodiode_sensor_main.cpp
+++ sensors/src/photodiode_sensor_main.cpp
@@ -1,47 +1,35 @@
 #include <ros/ros.h>
 #include <ros/duration.h>
+#include <string>
 
 #include "sensors/Photodiode.h"
 #include "sensors/GPIOClass.h"
 #include "topics/topics.h"
 
 // TODO: replace pin numbers
-GPIOClass* photodiode_1 = new GPIOClass("30");
-GPIOClass* photodiode_2 = new GPIOClass("31");
-GPIOClass* photodiode_3 = new GPIOClass("32");
-GPIOClass* photodiode_4 = new GPIOClass("33");
-GPIOClass* photodiode_5 = new GPIOClass("34");
+std::vector<GPIOClass*> photodiodes;
 
 void photodiodeInit() {
-    photodiode_1->export_gpio();
-    photodiode_1->setdir_gpio("in");
-
-    photodiode_2->export_gpio();
-    photodiode_2->setdir_gpio("in");
-
-    photodiode_3->export_gpio();
-    photodiode_3->setdir_gpio("in");
-
-    photodiode_4->export_gpio();
-    photodiode_4->setdir_gpio("in");
-
-    photodiode_5->export_gpio();
-    photodiode_5->setdir_gpio("in");
+    stringstream ss;
+    string pin;
+    for (int i = 0; i < 5; i++) {
+        // GPIO 2 to 6
+        ss << i+2;
+        pin = ss.str();
+        photodiodes[i] = new GPIOClass(pin);
+        photodiodes[i]->export_gpio();
+        photodiodes[i]->setdir_gpio("in");
+    }
 }
 
 sensors::Photodiode readPhotodiodeSensors() {
     sensors::Photodiode photodiode_data;
     photodiode_data.data.resize(5);
-    std::vector<std::string> photodiode_str(5, "");
 
-    photodiode_1->read_gpio(photodiode_str[0]);
-    photodiode_2->read_gpio(photodiode_str[1]);
-    photodiode_3->read_gpio(photodiode_str[2]);
-    photodiode_4->read_gpio(photodiode_str[3]);
-    photodiode_5->read_gpio(photodiode_str[4]);
-
-    for(int i = 0; i < photodiode_str.size(); i++) {
-        photodiode_data.data[i] = photodiode_str[i] == "1";
+    bool data;
+    for(int i = 0; i < photodiodes.size(); i++) {
+        photodiodes[i]->read_gpio(data);
+        photodiode_data.data[i] = data;
     }
 
     return photodiode_data;
