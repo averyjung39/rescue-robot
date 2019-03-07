741dda05eb9c68c67910dd91aea55c9605447ee3
Russell Wong <russwong89@gmail.com>
2019-03-06 21:41:16 -0500

    Comments

--- sensors/include/sensors/MotorEncoder.h
+++ sensors/include/sensors/MotorEncoder.h
@@ -8,9 +8,26 @@
 // Class for getting values from a motor encoder
 class MotorEncoder {
 public:
-    MotorEncoder(const char *pin_a, const char *pin_b, void (*isr_function)(void), volatile int *global_rising_edge_count);
+    /**
+     * @param pin_a, pin_b: pin numbers for motor encoder at A and B
+     * @param isr_function: pointer to ISR to run when a rising edge of A is detected
+     * @param global_rising_edge_count: pointer to integer counter for encoder values
+     */
+    MotorEncoder(const char *pin_a,
+        const char *pin_b,
+        void (*isr_function)(void),
+        volatile int *global_rising_edge_count);
     ~MotorEncoder();
+
+    /**
+     * @brief Returns motor position in degrees, with respect to wherever it was on startup
+     */
     float positionInDegrees() const;
+
+    /**
+     * @brief  Updates encoder count for motor
+     * @return false if unable to update count
+     */
     bool updateCount();
 private:
     static const int COUNTS_PER_REV = 64*30;
