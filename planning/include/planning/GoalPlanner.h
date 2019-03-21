c0e6f37d81d6c87bb95129b4913835926efebf21
Russell Wong <russwong89@gmail.com>
2019-03-21 19:53:29 -0400

    Implemented goal planner

--- planning/include/planning/GoalPlanner.h
+++ planning/include/planning/GoalPlanner.h
@@ -6,18 +6,18 @@
 #include "objectives/ActiveObjectives.h"
 
 enum RobotDirections {
-    UP = 0,
-    DOWN = 1,
-    LEFT = 2,
-    RIGHT = 3,
-    ANY = 4
+    ROBOT_DIRECTION_UP = 0,
+    ROBOT_DIRECTION_DOWN = 1,
+    ROBOT_DIRECTION_LEFT = 2,
+    ROBOT_DIRECTION_RIGHT = 3,
+    ROBOT_DIRECTION_ANY = 4
 };
 
 struct GoalPose {
     int i;
     int j;
     RobotDirections direction;
-}
+};
 
 class GoalPlanner {
 public:
@@ -30,12 +30,19 @@ private:
     bool _run_initialization;
     std::pair<int, int> _home_indices;
 
-    // Find all pair of indices that have a certain map cell type
-    std::vector<std::pair<int, int> > queryMap(const mapperception::Map &map, const MapEnum &cell_type) const;
+    // Find map indices that has a certain map cell type. Return the one closest to the robot
+    bool searchMap(const mapperception::Map &map, const int &label, const int &robot_i, const int &robot_j, std::pair<int, int> &indices) const;
 
     // Used when we need to set a GoalPose in which we want to face an obstacle
     RobotDirections getDirectionFacingCell(const std::pair<int, int> &curr_indices,
         const std::pair<int, int> &goal_indices) const;
+
+    // Helper function to determine an appropriate goal, e.g. when we want to go to a tile next to an object
+    std::pair<int, int> lowestCostIndicesNearby(const mapperception::Map &map, const int &i, const int &j, const int &robot_i, const int &robot_j) const;
+    // Determine a goal that will take the robot to the farthest side of the map
+    std::pair<int, int> farthestEdgeFromRobot(const int &robot_i, const int &robot_j) const;
+    // Determine a goal that will take the robot to the unsearched tile farthest away 
+    bool farthestUnsearchedCell(const mapperception::Map &map, const int &robot_i, const int &robot_j, std::pair<int, int> &indices) const;
 };
 
 #endif  // GOAL_PLANNER
--- planning/src/GoalPlanner.cpp
+++ planning/src/GoalPlanner.cpp
@@ -1,7 +1,9 @@
+#include "constants/objectives_list.h"
+#include "constants/labels.h"
 #include "planning/GoalPlanner.h"
 
 GoalPlanner::GoalPlanner() {
-    _run_initiaization = true;
+    _run_initialization = true;
     _home_indices = std::make_pair(5,3);
 }
 
@@ -9,66 +11,101 @@ GoalPose GoalPlanner::getGoal(
     const localization::Pose &pose,
     const mapperception::Map &map,
     const objectives::ActiveObjectives &objectives) {
-    std::vector<bool> objectives_list;
-
+    
     // TODO: Incorporate timeouts for some of these objectives
 
+    GoalPose goal_pose;
+    int robot_i = map.robot_i;
+    int robot_j = map.robot_j;
     // At the very beginning, sweep left and right
-    if (_run_initiaization) {
-        if (pose.theta < M_PI) {
+    if (_run_initialization) {
+        if (pose.theta < 180) {
             // Keep turning left. Set GoalPose to current indices in map, LEFT
-        } else if (pose.theta > 0 && pose.theta < 1.5*M_PI) {
+            goal_pose.i = robot_i;
+            goal_pose.j = robot_j;
+            goal_pose.direction = ROBOT_DIRECTION_LEFT;
+        } else if (pose.theta > 0 && pose.theta < 270) {
             // Start turning right. Set GoalPose to current indices in map, RIGHT
+            goal_pose.i = robot_i;
+            goal_pose.j = robot_j;
+            goal_pose.direction = ROBOT_DIRECTION_RIGHT;
         } else {
-            _run_initiaization = false;
+            _run_initialization = false;
         }
     } else {
+        std::vector<int> active_objectives = objectives.active_objectives;
+        
         // Start checking objectives
         // Want to find fire first, so check that first
-        if (objectives_list[objectives::FIND_FIRE]) {
+        std::pair<int, int> map_search_result;
+        std::pair<int, int> goal_indices;
+        if (active_objectives[objectives_list::FIND_FIRE]) {
             // If candle is in the map, set GoalPose to indices adjacent to the candle cell, direction facing the candle
             // Else if there is at least one unknown obstacle in the map, set GoalPose to indices adjacent to closest obstacle, direction facing obstacle
-            // Else set goal to some unsearched area
+            // Else set goal to some other area, like the fathest unsearched area or farthest edge of map
+            
+            if (searchMap(map, labels::FIRE, robot_i, robot_j, map_search_result)) {
+                goal_indices = lowestCostIndicesNearby(map, map_search_result.first, map_search_result.second, robot_i, robot_j);
+                goal_pose.direction = getDirectionFacingCell(goal_indices, map_search_result);
+            } else if (searchMap(map, labels::OBJECT, robot_i, robot_j, map_search_result) ||
+                searchMap(map, labels::TALL_OBJECT, robot_i, robot_j, map_search_result)) {
+                goal_indices = lowestCostIndicesNearby(map, map_search_result.first, map_search_result.second, robot_i, robot_j);
+                goal_pose.direction = getDirectionFacingCell(goal_indices, map_search_result);
+            } else if (farthestUnsearchedCell(map, robot_i, robot_j, map_search_result)) {
+                goal_indices = map_search_result;
+                goal_pose.direction = ROBOT_DIRECTION_ANY;
+            } else {
+                goal_indices = farthestEdgeFromRobot(robot_i, robot_j);
+                goal_pose.direction = ROBOT_DIRECTION_ANY;
+            }
         }
         // If we want to return home, this will be the only active objective, so we should just return home 
-        else if (objectives_list[objectives::RETURN_HOME]) {
+        else if (active_objectives[objectives_list::RETURN_HOME]) {
             // Set goal to home indices, orientation ANY
+            goal_indices = _home_indices;
+            goal_pose.direction = ROBOT_DIRECTION_ANY;
         } 
         // For all other active objectives, check if any of their locations have already been found in the map
-        else if (objectives_list[objectives::FIND_FOOD] && map.contains(magnet)) { // map.contains is pseudocode
+        else if (active_objectives[objectives_list::FIND_FOOD] && searchMap(map, labels::MAGNET, robot_i, robot_j, map_search_result)) {
             // Set goal to indices of closest magnet, orientation ANY
-        } else if (objectives_list[objectives::FIND_SURVIVORS] && map.contains(survivors)) {
+            goal_indices = map_search_result;
+            goal_pose.direction = ROBOT_DIRECTION_ANY;
+        } else if ((active_objectives[objectives_list::FIND_SURVIVORS] || active_objectives[objectives_list::RETURN_TO_SURVIVORS]) 
+            && searchMap(map, labels::BIG_HOUSE, robot_i, robot_j, map_search_result)) {
             // Set goal to indices adjacent to survivors cell, direction facing survivors
-        } else if (objectives_list[objectives::FIND_PERSON] && map.contains(person)) {
+            goal_indices = lowestCostIndicesNearby(map, map_search_result.first, map_search_result.second, robot_i, robot_j);
+            goal_pose.direction = getDirectionFacingCell(goal_indices, map_search_result);
+        } else if (active_objectives[objectives_list::FIND_PERSON] && searchMap(map, labels::SMALL_HOUSE, robot_i, robot_j, map_search_result)) {
             // Set goal to indices adjacent to person cell, direction facing person
+            goal_indices = lowestCostIndicesNearby(map, map_search_result.first, map_search_result.second, robot_i, robot_j);
+            goal_pose.direction = getDirectionFacingCell(goal_indices, map_search_result);
         }
         // If we haven't found the locations of these objectives, check unknown obstacles or unsearched areas
-        else if (map.contains(unknown_obstacle)) {
+        else if (searchMap(map, labels::TALL_OBJECT, robot_i, robot_j, map_search_result) ||
+            searchMap(map, labels::OBJECT, robot_i, robot_j, map_search_result)) {
             // Set goal to indices adjacent to closest unknown obstacle, direction facing obstacle
-        } else if (map.contains(unsearched_space)) {
-            // Set goal to indices of closest unsearched space, direction ANY
+            goal_indices = lowestCostIndicesNearby(map, map_search_result.first, map_search_result.second, robot_i, robot_j);
+            goal_pose.direction = getDirectionFacingCell(goal_indices, map_search_result);
+        } else if (farthestUnsearchedCell(map, robot_i, robot_j, map_search_result)) {
+            // Set goal to indices of farthest unsearched space, direction ANY
+            goal_indices = map_search_result;
+            goal_pose.direction = ROBOT_DIRECTION_ANY;
         } else {
             // Not sure what to do if we get to here. Probably print an error message and either spin on the spot or
             // try re-searching areas
 
             // Alternatively, we send a signal to objectives saying we weren't able to complete whatever objectives are currently listed
             // as active. Objectives then sends us a new list of objectives to do. (Implementing this would solve the issue of not having timeouts)
-        }
-    }
-}
 
-std::vector<std::pair<int, int> > GoalPlanner::queryMap(const mapperception::Map &map, const MapEnum &cell_type) const {
-    // For now, iterate through all map cells
-    // If this is found to be too slow, we can create other data structures for fast querying based on cell_type, like a std::unordered_map
-    std::vector<std::pair<int, int> > indices_list;
-    for (int i = 0; i < map.data.size(); ++i) {
-        mapperception::MapRow row = map.data[i];
-        for (int j = 0; j < row.data.size(); ++j) {
-            if (row.data[i] == cell_type) {
-                indices_list.push_back(std::make_pair(i,j));
-            }
+            // For now just go to the farthest edge of the map and hope that we see something new
+            goal_indices = farthestEdgeFromRobot(robot_i, robot_j);
+            goal_pose.direction = ROBOT_DIRECTION_ANY;
         }
+        goal_pose.i = goal_indices.first;
+        goal_pose.j = goal_indices.second;
     }
+
+    return goal_pose;
 }
 
 RobotDirections GoalPlanner::getDirectionFacingCell(const std::pair<int, int> &curr_indices,
@@ -76,10 +113,108 @@ RobotDirections GoalPlanner::getDirectionFacingCell(const std::pair<int, int> &c
     int i_diff = goal_indices.first - curr_indices.first;
     int j_diff = goal_indices.second - curr_indices.second;
 
-    if (i_diff == 1 && j_diff == 0) return RobotDirections::DOWN;
-    else if (i_diff == -1 && j_diff == 0) return RobotDirections::UP;
-    else if (i_diff == 0 && j_diff == 1) return RobotDirections::RIGHT;
-    else if (i_diff == 0 && j_diff == -1) return RobotDirections::LEFT;
+    if (i_diff == 1 && j_diff == 0) return ROBOT_DIRECTION_DOWN;
+    else if (i_diff == -1 && j_diff == 0) return ROBOT_DIRECTION_UP;
+    else if (i_diff == 0 && j_diff == 1) return ROBOT_DIRECTION_RIGHT;
+    else if (i_diff == 0 && j_diff == -1) return ROBOT_DIRECTION_LEFT;
         
-    return RobotDirections::ANY;
+    return ROBOT_DIRECTION_ANY;
+}
+
+bool GoalPlanner::searchMap(const mapperception::Map &map, const int &label, const int &robot_i, const int &robot_j, std::pair<int, int> &closest_indices) const {
+    std::vector<std::pair<int, int> > indices;
+    for (int i = 0; i < map.map.size(); ++i) {
+        std::vector<int> row = map.map[i].row;
+        for (int j = 0; j < row.size(); ++j) {
+            if (row[j] == label) {
+                indices.push_back(std::make_pair(i,j));
+            }
+        }
+    }
+
+    float min_distance = 100000;
+    for (int i = 0; i < indices.size(); ++i) {
+        float distance = sqrt((indices[i].first - robot_i)*(indices[i].first - robot_i) + (indices[i].second - robot_j)*(indices[i].second - robot_j));
+        if (distance < min_distance) {
+            min_distance = distance;
+            closest_indices = indices[i];
+        }
+    }
+    return indices.size() > 0;
+}
+
+std::pair<int, int> GoalPlanner::lowestCostIndicesNearby(const mapperception::Map &map,
+    const int &i,
+    const int &j,
+    const int &robot_i,
+    const int &robot_j) const {
+    std::pair<int, int> indices;
+    std::vector<std::pair<int, int> > indices_to_try;
+    if (i < map.map.size()-1) {
+        indices_to_try.push_back(std::make_pair(i+1,j));
+    }
+    if (i > 0) {
+        indices_to_try.push_back(std::make_pair(i-1,j));
+    }
+    if (j < map.map.size()-1) {
+        indices_to_try.push_back(std::make_pair(i,j+1));
+    }
+    if (j > 0) {
+        indices_to_try.push_back(std::make_pair(i,j-1));
+    }
+
+    float min_cost = 1000000;
+    for (int k = 0; k < indices_to_try.size(); ++k) {
+        int i_to_try = indices_to_try[k].first;
+        int j_to_try = indices_to_try[k].second;
+        float cost = map.map[i_to_try].row[j_to_try] + sqrt((i_to_try - robot_i)*(i_to_try - robot_i) + (j_to_try - robot_j)*(j_to_try - robot_j));
+        if (cost < min_cost) {
+            indices = indices_to_try[k];
+            min_cost = cost;
+        }
+    }
+
+    return indices;
+}
+
+std::pair<int, int> GoalPlanner::farthestEdgeFromRobot(const int &robot_i, const int &robot_j) const {
+    std::vector<std::pair<int, int> > indices_to_try;
+    indices_to_try.push_back(std::make_pair(0,2)); // All different possible start locations
+    indices_to_try.push_back(std::make_pair(2,5));
+    indices_to_try.push_back(std::make_pair(3,0));
+    indices_to_try.push_back(std::make_pair(5,3));
+
+    float max_distance = 0;
+    std::pair<int, int> indices;
+    for (int i = 0; i < indices_to_try.size(); ++i) {
+        float distance = sqrt((indices_to_try[i].first - robot_i)*(indices_to_try[i].first - robot_i) + (indices_to_try[i].second - robot_j)*(indices_to_try[i].second - robot_j));
+        if (distance > max_distance) {
+            max_distance = distance;
+            indices = indices_to_try[i];
+        }
+    }
+    return indices;
+}
+
+bool GoalPlanner::farthestUnsearchedCell(
+    const mapperception::Map &map,
+    const int &robot_i,
+    const int &robot_j,
+    std::pair<int, int> &indices) const {
+    float max_distance = 0;
+    bool found = false;
+    for (int i = 0; i < map.map.size(); ++i) {
+        std::vector<int> row = map.map[i].row;
+        for (int j = 0; j < row.size(); ++j) {
+            if (row[j] == labels::UNSEARCHED) {
+                found = true;
+                float distance = sqrt((i - robot_i)*(i - robot_i) + (j - robot_j)*(j - robot_j));
+                if (distance > max_distance) {
+                    max_distance = distance;
+                    indices = std::make_pair(i,j);
+                }
+            }
+        }
+    }
+    return found;
 }
