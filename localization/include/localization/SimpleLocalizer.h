0e1f5f92f48df830913fdc4b4a270f2ae68f53d1
Russell Wong <russwong89@gmail.com>
2019-03-21 22:24:18 -0400

    New Localization (#48)
    * Updated localization to only use time-based driving
    
    * Fixed bug

--- localization/include/localization/SimpleLocalizer.h
+++ localization/include/localization/SimpleLocalizer.h
@@ -13,9 +13,9 @@ public:
         const float &right_turn_time,
         const float &left_turn_time);
     localization::Pose getPoseEstimate(
-        const messages::Arc &arc_msg,
-        const float &imu_yaw,
-        const std::vector<float> &high_distance_data);
+        const messages::Arc &arc_msg);
+        // const float &imu_yaw,
+        //const std::vector<float> &high_distance_data);
 
 private:
     static const int FRONT_INDEX = 0;
@@ -24,12 +24,12 @@ private:
 
     localization::Pose _current_pose;
     int _prev_control_command;
-    float _starting_imu_yaw;
+    // float _starting_imu_yaw;
     float _starting_angle_deg;
-    float _front_distance_cm;
-    float _back_distance_cm;
-    float _prev_front_distance_cm;
-    float _prev_back_distance_cm;
+    // float _front_distance_cm;
+    // float _back_distance_cm;
+    // float _prev_front_distance_cm;
+    // float _prev_back_distance_cm;
     int _nominal_theta_deg; // Closest angle to yaw that's a multiple of 90 (when driving straight)
     float _nominal_x_cm;
     float _nominal_y_cm;
